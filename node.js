64c944e784feaed428319e4c5dedf0aed345d46f
ruma684 <jatinzuro@gmail.com>
2025-10-18 10:39:31 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,6 @@
+ window.addEventListener("load", function(){
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+      document.getElementById("contents").style.display = "block";
+    }, 3000);
+  });
